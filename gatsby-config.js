8e51e2940b42f119d4f44c45ccf5253da7149fa4
Victor Moreliere <mrcola57@gmail.com>
2022-07-10 12:26:47 +0200

    Add basic GatsbyJS starter

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `Victor Moreliere's portfolio`,
+    description: `My personal portfolio. You will find information of my education, career path and all my project / information of the 1 Month 1 project initiative (1M1P).`,
+    author: `Victor Moreliere`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassOptions: {
+          precision: 6,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-bootstrap-5`,
+        short_name: `gb5-starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+  ],
+}
